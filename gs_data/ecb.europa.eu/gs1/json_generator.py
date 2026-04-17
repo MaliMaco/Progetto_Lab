3bef6e8a60f7cd1d2a2fc34c4b2a27b6fd096482
MaliMaco <m.grigoli.26@gmail.com>
2026-04-17 21:35:27 +0200

    Errore in json_generator.py in gs1.

--- gs_data/ecb.europa.eu/gs1/json_generator.py
+++ gs_data/ecb.europa.eu/gs1/json_generator.py
@@ -11,16 +11,21 @@ html_text = html_file.read()
 gs_text = gs_file.read()
 
 gs_text = unicodedata.normalize('NFKC', gs_text)
-gs_text = re.sub(r'\u00f9(?=[A-ZÀÈÉÌÒÙ])', ' ', gs_text)
-gs_text = re.sub(r'[\u2018\u2019\u201a\u201b]', "'", gs_text)
+gs_text = re.sub(r'[\u2018\u2019\u201a\u201b\u2032]', "'", gs_text)
 gs_text = re.sub(r'[\u201c\u201d\u201e\u201f]', '"', gs_text)
 gs_text = re.sub(r'[\u2013\u2014\u2015]', '-', gs_text)
-gs_text = re.sub(r'[\u00a0\u202f\u2009\u2008\u2007\u2006\u2005\u2004\u2003\u2002]', ' ', gs_text)
+gs_text = re.sub(r'[^\x00-\x7F\u00C0-\u024F\u20AC\u00e8]', '', gs_text)
+gs_text = re.sub(r'(?<=[^\s])\u00f9(?=[A-ZÀÈÉÌÒÙ])', ' ', gs_text)
 gs_text = re.sub(r'\s+', ' ', gs_text).strip()
 
-pattern_title = r'<(?:h[1-6]|div\s+class=["\']title["\'])[^>]*>\s*(?:<[^>]+>)?(.*?)(?:<\/[^>]+>)?\s*<\/(?:h[1-6]|div)>'
+pattern_title = r'<title>(.*?)</title>'
 match = re.search(pattern_title, html_text)
 title = match.group(1)
+title = re.sub(r'[\u2018\u2019\u201a\u201b\u2032]', "'", title)
+title = re.sub(r'[\u201c\u201d\u201e\u201f]', '"', title)
+title = re.sub(r'[\u2013\u2014\u2015]', '-', title)
+title = re.sub(r'[^\x00-\x7F\u00C0-\u024F\u20AC\u00e8]', '', title)
+title = re.sub(r'(?<=[^\s])\u00f9(?=[A-ZÀÈÉÌÒÙ])', ' ', title)
 
 pattern_domain = r'^(?:https?://)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,})(?::\d+)?'
 match = re.search(pattern_domain, "https://www.ecb.europa.eu/mopo/intro/benefits/html/index.it.html")

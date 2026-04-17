a08049f2666b7eca2224531e861267b335ca1b9d
MaliMaco <m.grigoli.26@gmail.com>
2026-04-17 15:36:45 +0200

    Aggiornamento e correzzione errore di un dominio.

--- backend/src/server.py
+++ backend/src/server.py
@@ -10,6 +10,8 @@ from pathlib import Path
 
 app = FastAPI(title="Backend API")
 
+pattern_domain = r'^(?:https?://)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,})(?::\d+)?'
+domains_path = os.path.join(Path(__file__).parent.parent, 'domains.json')
 
 class ParseOutput(BaseModel):
     url: str
@@ -25,19 +27,27 @@ class GSResponse(BaseModel):
     html_text: str
     gold_text: str
 
+class FullGSResponse(BaseModel):
+    gold_standard: List[Dict[str]]
+
 class DomainsResponse(BaseModel):
     domains: List[str]
 
 
 @app.get("/parse")
 def parse(url: str) -> ParseOutput:
+    if result.status_code == 404:
+        return HTTPException(status_code=404, detail="URL irrangiugibile.")
     result = asyncio.run(parser_run(url))
     pattern_title = r'<title>(.*?)</title>'
     match = re.search(pattern_title, result.html)
     title = match.group(1)
-    pattern_domain = r'^(?:https?://)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,})(?::\d+)?'
     match = re.search(pattern_domain, url)
     domain = match.group(1)
+    with open(domains_path, 'r') as dm_file:
+        domains = dm_file.read()
+        if domain not in domains['domains']:
+            return HTTPException(status_code=400, detail="Dominio non supportato.")
     html_text = result.html
     md_text = result.markdown
     md_text = re.sub(r'\(\s*https?://[^)]*\)', ' ', md_text)
@@ -53,12 +63,8 @@ def parse(url: str) -> ParseOutput:
     
 @app.get("/domains")
 def get_domains() -> DomainsResponse:
-    domini = [
-        "en.wikipedia.org",
-        "www.ecb.europa.eu",
-        "apps.apple.com",
-        "tandfonline.com"
-            ]
+    with open(domains_path, 'r') as dm_file:
+        domini = dm_file.read()
     return DomainsResponse(
         domains=domini
         )
@@ -66,10 +72,8 @@ def get_domains() -> DomainsResponse:
 
 @app.get("/gold_standard")
 def get_gold_standard(url:str):
-    pattern_domain = r'^(?:https?://)?(?:www\.)?([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,})(?::\d+)?'
     match = re.search(pattern_domain, url)
     domain = match.group(1)
-    domains_path = os.path.join(Path(__file__).parent.parent, 'domains.json')
     with open(domains_path, 'r') as dm_file:
         domains = dm_file.read()
         if domain not in domains['domains']:
@@ -80,17 +84,25 @@ def get_gold_standard(url:str):
         for gs in gs_list:
             if gs['url'] == url:
                 return GSResponse(url=url,
-                                title=gs['title'], 
-                                domain=gs['domain'],
-                                html_text=gs['html_text'], 
-                                gold_text=gs['gold_text']
+                            title=gs['title'], 
+                            domain=gs['domain'],
+                            html_text=gs['html_text'], 
+                            gold_text=gs['gold_text']
                                 )
-        return HTTPException(status_code=400, detail="url non presente nel GS.")
+        return HTTPException(status_code=400, detail="URL non presente nel GS.")
     
 
 @app.get("/full_gold_standard")
-def get_full_gold_standard():
-    pass
+def get_full_gold_standard(domain: str):
+    with open(domains_path, 'r') as dm_file:
+        domains = dm_file.read()
+        if domain not in domains['domains']:
+            return HTTPException(status_code=400, detail="Dominio non supportato.")
+        
+    GS_path = os.path.join(Path(__file__).parent.parent,f"gs_data/{domain}/GS.json")
+    with open(GS_path, 'r', encoding="UTF-8") as GS_file:
+        golden_standard = GS_file.read()
+        return FullGSResponse(gold_standard=golden_standard)
 
 @app.post("/evaluate")
 def evaluate():

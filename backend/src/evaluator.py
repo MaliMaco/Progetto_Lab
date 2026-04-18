78ee7cff2929a30973de686973cf37dce2951195
MaliMaco <m.grigoli.26@gmail.com>
2026-04-18 02:19:12 +0200

    Pattern title corretto, server di logica pienamente implementato.

--- backend/src/evaluator.py
+++ backend/src/evaluator.py
@@ -1,5 +1,3 @@
-import json
-import re
 from typing import Dict
 
 class TokenEvaluator:
@@ -21,5 +19,18 @@ class TokenEvaluator:
         return parsed_tokens
 
     @staticmethod
-    def evaluate(parsed_text: str, gold_text: str) -> Dict[str,Dict[int]]:
-        pass
+    def evaluate(parsed_text: set[str], gold_text: set[str]) -> Dict[str,Dict[int]]:
+        intersect = parsed_text.intersection(gold_text)
+        intersect_length = len(intersect)
+        parsed_length = len(parsed_text)
+        gold_length = len(gold_text)
+        precision = intersect_length/parsed_length
+        recall = intersect_length/gold_length
+        f1 = 2*precision*(recall/(precision+recall))
+        return {
+            "token_level_eval": {
+                "precision": precision,
+                "recall": recall,
+                "f1": f1
+            }
+        }
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Set
 from crawler import parser_run
 import asyncio
 import re
 import os
+import json
 from pathlib import Path
+from evaluator import TokenEvaluator
 
 
 app = FastAPI(title="Backend API")
@@ -28,10 +30,17 @@ class GSResponse(BaseModel):
     gold_text: str
 
 class FullGSResponse(BaseModel):
-    gold_standard: List[Dict[str]]
+    gold_standard: List[Dict[str,str]]
 
 class DomainsResponse(BaseModel):
     domains: List[str]
+
+class EvaluateRequest(BaseModel):
+    parsed_text: str
+    gold_text: str
+
+class EvaluateResponse(BaseModel):
+    token_level_eval: Dict[str,Dict[str,int]]
 
 
 @app.get("/parse")
@@ -39,13 +48,14 @@ def parse(url: str) -> ParseOutput:
     if result.status_code == 404:
         return HTTPException(status_code=404, detail="URL irrangiugibile.")
     result = asyncio.run(parser_run(url))
-    pattern_title = r'<title>(.*?)</title>'
+    pattern_title = r'<(?:title|(?:div\s+class=["\'](?:title|header)["\'][^>]*>\s*<h[1-6]))[^>]*>\s*(?:<[^>]+>)?\s*(.*?)\s*(?:</[^>]+>)?\s*</(?:title|h[1-6])>'
     match = re.search(pattern_title, result.html)
     title = match.group(1)
     match = re.search(pattern_domain, url)
     domain = match.group(1)
     with open(domains_path, 'r') as dm_file:
-        domains = dm_file.read()
+        text = dm_file.read()
+        domains = json.load(text)
         if domain not in domains['domains']:
             return HTTPException(status_code=400, detail="Dominio non supportato.")
     html_text = result.html
@@ -58,13 +68,14 @@ def parse(url: str) -> ParseOutput:
         domain=domain, 
         title=title, 
         html_text=html_text, 
-        md_text=md_text
+        parsed_text=md_text
     )
     
 @app.get("/domains")
 def get_domains() -> DomainsResponse:
     with open(domains_path, 'r') as dm_file:
-        domini = dm_file.read()
+        text = dm_file.read()
+        domini = json.load(text)
     return DomainsResponse(
         domains=domini
         )
@@ -75,39 +86,76 @@ def get_gold_standard(url:str):
     match = re.search(pattern_domain, url)
     domain = match.group(1)
     with open(domains_path, 'r') as dm_file:
-        domains = dm_file.read()
+        text = dm_file.read()
+        domains = json.load(text)
         if domain not in domains['domains']:
             return HTTPException(status_code=400, detail="Dominio non supportato.")
     GS_path = os.path.join(Path(__file__).parent.parent,f"gs_data/{domain}/GS.json")
     with open(GS_path, 'r', encoding="UTF-8") as GS_file:
-        gs_list = GS_file.read()
-        for gs in gs_list:
-            if gs['url'] == url:
+        gs = GS_file.read()
+        for single_gs in gs:
+            true_gs = json.load(single_gs)
+            if true_gs['url'] == url:
                 return GSResponse(url=url,
-                            title=gs['title'], 
-                            domain=gs['domain'],
-                            html_text=gs['html_text'], 
-                            gold_text=gs['gold_text']
+                            title=true_gs['title'], 
+                            domain=true_gs['domain'],
+                            html_text=true_gs['html_text'], 
+                            gold_text=true_gs['gold_text']
                                 )
         return HTTPException(status_code=400, detail="URL non presente nel GS.")
     
 
 @app.get("/full_gold_standard")
-def get_full_gold_standard(domain: str):
+def get_full_gold_standard(url: str):
+    match = re.search(pattern_domain, url)
+    domain = match.group(1)
     with open(domains_path, 'r') as dm_file:
-        domains = dm_file.read()
+        text = dm_file.read()
+        domains = json.load(text)
         if domain not in domains['domains']:
             return HTTPException(status_code=400, detail="Dominio non supportato.")
         
     GS_path = os.path.join(Path(__file__).parent.parent,f"gs_data/{domain}/GS.json")
+    full_gs = []
     with open(GS_path, 'r', encoding="UTF-8") as GS_file:
-        golden_standard = GS_file.read()
-        return FullGSResponse(gold_standard=golden_standard)
+        gs = GS_file.read()
+        for gs_elem in gs:
+            golden_standard = json.load(gs_elem)
+            full_gs.append(golden_standard)
+        return FullGSResponse(gold_standard=full_gs)
 
 @app.post("/evaluate")
-def evaluate():
-    pass
+def evaluate(request: EvaluateRequest) -> EvaluateResponse:
+    parsed_set = TokenEvaluator.token_parsed_text(request.parsed_text)
+    gold_set = TokenEvaluator.token_gold_text(request.gold_text)
+    payload = TokenEvaluator.evaluate(parsed_text=parsed_set,gold_text=gold_set)
+    return EvaluateResponse(token_level_eval=payload)
 
 @app.get("/full_gs_eval")
-def get_full_gs_eval():
-    pass
+def get_full_gs_eval(url: str) -> EvaluateResponse:
+    full_gs_response = get_full_gold_standard(url=url)
+    sum_precision = 0
+    sum_recall = 0
+    sum_f1 = 0
+    gs_number = 0
+    for gs in full_gs_response:
+        result = parse(url)
+        evaluation = evaluate(EvaluateRequest(result.parsed_text, gs['gold_text']))
+        sum_precision += evaluation.token_level_eval.get("precision")
+        sum_recall += evaluation.token_level_eval.get("recall")
+        sum_f1 += evaluation.token_level_eval.get("f1")
+        gs_number += 1
+
+    precision = sum_precision/gs_number
+    recall = sum_recall/gs_number
+    f1 = sum_f1/gs_number
+
+    full_gs_token_eval = {
+        "token_level_eval": {
+            "precision": precision,
+            "recall": recall,
+            "f1": f1
+        }
+    }
+
+    return EvaluateResponse(token_level_eval=full_gs_token_eval)

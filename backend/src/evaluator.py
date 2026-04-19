1c18f9f43a650336737b586ea73d69d5df9913e0
MaliMaco <m.grigoli.26@gmail.com>
2026-04-19 02:05:20 +0200

    Server di logica terminato, Dockerfile iniziati ma non terminati.

--- backend/src/evaluator.py
+++ backend/src/evaluator.py
@@ -1,9 +1,9 @@
-from typing import Dict
+from typing import Dict, Set
 
 class TokenEvaluator:
 
     @staticmethod
-    def token_parsed_text(parsed_text: str) -> set[str]:
+    def token_parsed_text(parsed_text: str) -> Set[str]:
         tokens = parsed_text.split(" ")
         parsed_tokens = set()
         for token in tokens:
@@ -11,7 +11,7 @@ class TokenEvaluator:
         return parsed_tokens
 
     @staticmethod
-    def token_gold_text(gold_text: str) -> set[str]:
+    def token_gold_text(gold_text: str) -> Set[str]:
         tokens = gold_text.split(" ")
         parsed_tokens = set()
         for token in tokens:
@@ -19,14 +19,14 @@ class TokenEvaluator:
         return parsed_tokens
 
     @staticmethod
-    def evaluate(parsed_text: set[str], gold_text: set[str]) -> Dict[str,Dict[int]]:
+    def evaluate(parsed_text: Set[str], gold_text: Set[str]) -> Dict[str,Dict[str,float]]:
         intersect = parsed_text.intersection(gold_text)
-        intersect_length = len(intersect)
+        intersection_length = len(intersect)
         parsed_length = len(parsed_text)
         gold_length = len(gold_text)
-        precision = intersect_length/parsed_length
-        recall = intersect_length/gold_length
-        f1 = 2*precision*(recall/(precision+recall))
+        precision = intersection_length/parsed_length
+        recall = intersection_length/gold_length
+        f1 = 2*precision*recall/(precision+recall)
         return {
             "token_level_eval": {
                 "precision": precision,

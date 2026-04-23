d2683ae25cb3ffd166723cd93670f9ee43e30dfb
Daniele Antonacci <antonacci.2128556@studenti.uniroma1.it>
2026-04-23 23:20:31 +0200

    commit

---
+++ gs_data/tandfonline.com/gs2/parser_tand_facilitating.py
@@ -0,0 +1,44 @@
+import asyncio
+from crawl4ai import AsyncWebCrawler
+from crawl4ai.async_configs import BrowserConfig, CrawlerRunConfig
+import os
+
+html_path = os.path.join(os.path.dirname(__file__), "html_tand_facilitating.txt")
+
+
+async def main():
+    # Configurazione browser REALISTICA per evitare 403
+    browser_config = BrowserConfig(
+        headless=True,
+        viewport_width=1280,
+        viewport_height=720,
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    )
+    
+    # Configurazione con header aggiuntivi
+    run_config = CrawlerRunConfig(
+        target_elements=["h1", "h2", "h3", "title", "p", 
+                         "main", ".main-content", "article"],
+        wait_until="networkidle",
+        verbose=True,
+        
+        
+        
+    )
+    
+    async with AsyncWebCrawler(config=browser_config) as crawler:
+        result = await crawler.arun(
+            url="https://www.tandfonline.com/doi/full/10.1080/18387357.2026.2661084#d1e668",
+            config=run_config
+        )
+        
+        if result.success:
+            # Salva l'HTML
+            with open(html_path, 'w', encoding='utf-8') as f:
+                f.write(result.html)
+            print(f"✅ HTML salvato in: {html_path}")
+            
+            
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ gs_data/tandfonline.com/gs4/README.txt
@@ -0,0 +1,7 @@
+nota per beautiful soup: tagliare da
+(dopo conclusion)
+Declaration of generative AI and AI-assisted technologies in the writing process
+During the preparation of this work, the author(s) used ChatGPT in order to assist in drafting, structuring, and refining the text, as well as for language improvements. After using this tool/service, the author(s) reviewed and edited the content as needed and take(s) full responsibility for the content of the published article.
+ 
+inoltre runnare il parser perché manca html da dare al json generator.
+e runnare poi json generator (già pronto)
